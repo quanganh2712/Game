54ee2d3523b456cf098c404f5c1c716df2968fbd
quanganh <anhdqse184150@fpt.edu.vn>
2026-03-25 09:58:13 +0700

    fix

--- lib/mongodb.ts
+++ lib/mongodb.ts
@@ -1,6 +1,6 @@
 import { MongoClient } from "mongodb";
 
-const uri = process.env.MONGODB_URI;
+const uri = process.env.MONGO_URL;
 
 if (!uri) {
   throw new Error("Please add MONGODB_URI to .env.local or Vercel secrets");
